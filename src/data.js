c86d5dc3ce65fd233cbe8e944886a06b9b8789ff
krlos <lovecraft90@outlook.es>
2022-10-24 22:27:22 -0500

    Proyecto Terminado

--- src/data.js
+++ src/data.js
@@ -27,4 +27,60 @@ let shopItemsData = [
         price: 250,
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
         img: "images/img-4.jpg"
+    },
+    {
+        id: "shop05",
+        name: "Corbata",
+        price: 45,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-5.png"
+    },
+    {
+        id: "shop06",
+        name: "Snickers",
+        price: 180,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-6.png"
+    },
+    {
+        id: "shop07",
+        name: "Saco",
+        price: 185,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-7.png"
+    },
+    {
+        id: "shop08",
+        name: "Polo",
+        price: 85,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-8.png"
+    },
+    {
+        id: "shop09",
+        name: "Casaca Jean",
+        price: 250,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-9.png"
+    },
+    {
+        id: "shop10",
+        name: "Pantalón",
+        price: 100,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-10.png"
+    },
+    {
+        id: "shop11",
+        name: "Gorro",
+        price: 95,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-11.png"
+    },
+    {
+        id: "shop12",
+        name: "Botas",
+        price: 350,
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        img: "images/img-12.png"
     }]
